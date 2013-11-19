d4f5e8858a97181809d083199ac7fe80dc9564b7
jython234 <example@example.com>
2013-11-19 17:49:04 -0600

    Changed server.java

--- src/MineJava-Server/server.java
+++ src/MineJava-Server/server.java
@@ -1,12 +1,22 @@
 //Main server file
 //Need main class MineJavaServer
 
-class MineJavaServer {
+class MineJavaServer implements Runnable{
+	//Main server thread
 	
-	public static void main(String[] args){
+	public static void run(){
 		//Main method
-		//Used when program is launched
-		
+		//This function is called when the thread is started
+		ip=
+		this.startServer(ip, port);
 	}
 	
+}
+
+class Server {
+	
+	public static void main(String [] args){
+		//Run on startup
+	}
+
 }
