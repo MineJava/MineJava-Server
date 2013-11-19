ef90fa4cc57f236cc28e088d9a09f88957882297
jython234 <atzeise@gmail.com>
2013-11-19 17:29:06 -0600

    Created src and MineJava package, also included server.java

---
+++ MineJava-Server/server.java
@@ -0,0 +1,12 @@
+//Main server file
+//Need main class MineJavaServer
+
+class MineJavaServer {
+	
+	public static void main(String[] args){
+		//Main method
+		//Used when program is launched
+		
+	}
+	
+}
