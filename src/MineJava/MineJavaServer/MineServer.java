38f5ff8193a7968cbe2833a6c39eb553ddcaf97b
jython234 <jython234@hotmail.com>
2014-01-16 16:27:08 -0600

    Added MineServer.java
    Added new package; new server class.

---
+++ src/MineJava/MineJavaServer/MineServer.java
@@ -0,0 +1,11 @@
+package MineJava.MineJavaServer.MineServer;
+import java.util.*;
+import java.net.*;
+
+class MineServer implements Runnable{
+	//Main server thread
+	public void run(){
+		//Main thread startup
+		//TODO: Get sockets, Get logger init
+	}
+}
