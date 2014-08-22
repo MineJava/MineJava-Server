910da99a17f6874f701c5b1e6c41cefcda268cc1
jython234 <jython234@users.noreply.github.com>
2014-08-22 15:24:30 -0500

    PONG is 0x03 :D

--- src/MineJava/MineJavaServer/networking/packets/PacketConst.java
+++ src/MineJava/MineJavaServer/networking/packets/PacketConst.java
@@ -4,8 +4,8 @@ package MineJava.MineJavaServer.networking.packets;
 public class PacketConst{
 
 	//// packet values
-	public final static int PING=0x0;
-	public final static int PONG=0x1;
+	public final static int PING=0x00;
+	public final static int PONG=0x03;
 
 	public final static int MOVE_YAW=0x93,
 		MOVE=0x94
